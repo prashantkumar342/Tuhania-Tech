aebd7c5da057ff44b435eae6e7445cb4f4b9c4a5
Prashant Kumar <105093586+prashantkumar342@users.noreply.github.com>
2024-01-15 18:25:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function sideMenu() {
+  document.querySelector("#navigation-menu").classList.add("menu-active");
+}
+function sideMenuClose() {
+  document.querySelector("#navigation-menu").classList.remove("menu-active");
+}
